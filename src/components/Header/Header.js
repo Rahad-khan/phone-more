0aaa34658e5dc6c64e52f971272ce1634d3a40b8
Rahad khan <clash146098@gmail.com>
2022-04-04 00:27:20 +0600

    hero Area done with response

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import CustomLink from "../CustomLink/CustomLink";
+import { CgMenu } from "react-icons/cg";
+import { ImCross } from "react-icons/im";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
+      <nav className="bg-white border-gray-200 sm:py-2.5 rounded">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
         <Link to="/home" className="flex items-center">
             <p className="text-2xl font-semibold">Phone 
@@ -20,29 +22,7 @@ const Header = () => {
           className="block md:hidden"
           >
             {
-              open ? <svg
-              className="w-8 h-8"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg> : <svg
-              className="w-8 h-8"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+              open ? <ImCross/> : <CgMenu/>
             }
           </button>
           <div className={`w-full md:block md:w-auto ${open ? 'block' : 'hidden'}`}>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { HiAdjustments } from "react-icons/hi";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>yo</h1>
-            <p><HiAdjustments className='text-6xl'/> </p>
+  return (
+    <main>
+      {/* Hero Area */}
+      <section>
+        <div className="flex flex-col md:flex-row justify-between items-center md:my-5">
+          <div className="my-5 md:my-0 text-3xl md:text-6xl font-bold">
+            <h1 className="text-orange-700">YOUR NEXT PHONE</h1>
+            <h1 className="">YOUR BEST PHONE</h1>
+          </div>
+          <div className="flex justify-center">
+            <img
+              className="w-3/6 md:w-5/6"
+              src={require("../../image/apple.jpg")}
+              alt=""
+            />
+          </div>
         </div>
-    );
+      </section>
+      <section>
+          
+      </section>
+    </main>
+  );
 };
 
 export default Home;
